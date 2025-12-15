2f37cdb7a81c5d3a870df2e2c535ca560013e6d4
p-majchrzak <piotr-majchrzak@outlook.com>
2025-12-15 14:35:32 +0100

    Initial Commit

---
+++ PMajchrzak_WDP_Projekt/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PMajchrzak_WDP_Projekt
+//
+//  Created by Piotr Majchrzak on 15/12/2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
