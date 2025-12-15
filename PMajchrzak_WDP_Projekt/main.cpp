77f17ef446e0087fc4d0e8878245052a135b1713
p-majchrzak <piotr-majchrzak@outlook.com>
2025-12-15 14:47:53 +0100

    Dodano definicję struktury odpowiedzialnej za lot.

--- PMajchrzak_WDP_Projekt/main.cpp
+++ PMajchrzak_WDP_Projekt/main.cpp
@@ -1,14 +1,5 @@
-//
-//  main.cpp
-//  PMajchrzak_WDP_Projekt
-//
-//  Created by Piotr Majchrzak on 15/12/2025.
-//
-
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return EXIT_SUCCESS;
+int main() {
+    
 }
---
+++ PMajchrzak_WDP_Projekt/rezerwacja.h
@@ -0,0 +1,9 @@
+#definie MIASTO 25
+
+struct Lot{
+    int id_lotu;
+    char miasto_odlotu[MIASTO];
+    char miasto_przylotu[MIASTO];
+    int dostepne_miejsca;
+    double cena;
+}
