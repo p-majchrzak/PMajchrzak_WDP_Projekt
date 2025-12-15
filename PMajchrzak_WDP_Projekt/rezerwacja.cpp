3d21f7b41ee40907d6a37a350649b03a06baabd5
p-majchrzak <piotr-majchrzak@outlook.com>
2025-12-15 15:14:15 +0100

    Dodano funkcję do inicjalizacji bazy danych.

--- PMajchrzak_WDP_Projekt/rezerwacja.cpp
+++ PMajchrzak_WDP_Projekt/rezerwacja.cpp
@@ -5,5 +5,20 @@
 
 void stworzBazeDanych()
 {
-    FILE *loty = fopen()
+    FILE *loty = fopen(BAZA_LOTY, "ab");
+    FILE *rezerwacje = fopen(BAZA_REZERWACJE, "ab");
+    
+    if(rezerwacje == nullptr || loty == nullptr)
+    {
+        printf("Blad: \n Nie udalo sie otworzyc/utworzyc pliku.");
+        if(rezerwacje) fclose(rezerwacje);
+        if(loty) fclose(loty);
+        return;
+    }
+    else{
+        printf("Bazy danych zostaly wczystane/zaladowane.");
+    }
+    fclose(rezerwacje);
+    fclose(loty);
+    
 }
