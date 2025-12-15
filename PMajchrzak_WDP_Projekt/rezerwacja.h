fb41c0ea6759b1e83cac2ec4498a8d4d9cac9a1c
p-majchrzak <piotr-majchrzak@outlook.com>
2025-12-15 14:56:43 +0100

    Dodano deklaracje funkcji, struktury Rezerwacja, oraz definicję plików binarnych.

--- PMajchrzak_WDP_Projekt/rezerwacja.h
+++ PMajchrzak_WDP_Projekt/rezerwacja.h
@@ -1,4 +1,9 @@
+#definie BAZA_LOTY "loty.bin"
+#definie BAZA_REZERWACJE "rezerwacje.bin"
+
 #definie MIASTO 25
+#definie IMIE 25
+#definie NAZWISKO 25
 
 struct Lot{
     int id_lotu;
@@ -7,3 +12,26 @@ struct Lot{
     int dostepne_miejsca;
     double cena;
 }
+
+struct Rezerwacja{
+    int id_rezerwacja;
+    int id_lotu;
+    char imie[IMIE];
+    char nazwisko[NAZWISKO];
+}
+
+void stworzBazeDanych();
+
+void wyswietlLoty();
+
+void znajdzLot();
+
+void rezerwacjaBiletu();
+
+void dodajLot();
+
+void usunLot();
+
+void edytujLot();
+
+void listaPasazerow();
